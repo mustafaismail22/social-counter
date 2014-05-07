7212563bfcdc6b8bf8adf78e3e7488a63d474710
Mustafa ismail <mustafaismail22@users.noreply.github.com>
2014-05-07 21:25:09 +0300

    Create social-counter-widget.php

---
+++ social-counter-widget/social-counter-widget.php
@@ -0,0 +1,15 @@
+<?php
+
+/*
+Plugin Name: Social-Counter
+Plugin URI: http://demo.mustafaismail.co/social/
+Description: 
+Version: 0.7
+Author: Mustafa ismail
+Author URI:http://mustafaismail.co/
+License: GPLv2 or later
+*/
+
+
+include 'social-counter-class.php';
+include 'widget.php';
